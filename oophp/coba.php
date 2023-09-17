ff389cb4616b1fbd96ba2b18e68c57bc86d6c031
Fadlyyyy <fadlyynasah86@f=gmail.com>
2023-09-17 13:06:46 +0700

    class dan object

---
+++ oophp/coba.php
@@ -0,0 +1,15 @@
+<?php 
+
+class coba {
+
+}
+
+$a = new coba();
+$b = new coba();
+$b = new coba ();
+
+var_dump($a);
+var_dump($b);
+var_dump($b);
+
+?>
---
+++ oophp/produk.php
@@ -0,0 +1,18 @@
+<?php
+
+
+// jualan produk
+//  komik
+//  game
+
+class produk {
+
+}
+
+
+$produk1 = new produk();
+$produk2 = new produk();
+
+
+
+?>
