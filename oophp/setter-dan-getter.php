b071c499623bc73bacba89faba4f3d1e032e5b94
Fadlyyyy <fadlyynasah86@f=gmail.com>
2023-09-25 13:19:28 +0700

    about setter dan getter

--- oophp/setter-dan-getter.php
+++ oophp/setter-dan-getter.php
@@ -2,14 +2,11 @@
 
 class Produk {
 
-    public  $judul,
+    private  $judul,
             $penulis,
-            $penerbit;
-
-    protected  $diskon;
-
-
-    private $harga;
+            $penerbit,
+            $harga,
+            $diskon = 0;
 
     public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit ",
      $harga = 0) {
@@ -20,7 +17,49 @@ class Produk {
         $this->penerbit = $penerbit;
         $this->harga = $harga;
     }
-            
+
+
+    public function setJudul($judul) {
+        // if(!is_string($judul)) {
+        //     throw new Exception("Judul Harus String");
+        // }
+        $this->judul = $judul;
+    }
+
+    public function getJudul() {
+        return $this->judul;
+    }
+
+    public function setPenulis($penulis) {
+        $this->penulis = $penulis;
+    }
+
+    public function getPenulis(){
+        return $this->penulis;
+    }
+
+    public function setPenerbit($penerbit) {
+        $this->penerbit = $penerbit;
+    }
+
+    public function getPenerbit() {
+        return $this->penerbit;
+    }
+     
+    public function setDiskon ($diskon) {
+        $this->diskon = $diskon;
+    }
+
+    public function getDiskon () {
+        return $this->diskon;
+    }
+
+
+    public function setHarga($harga) {
+        $this->harga = $harga;
+    }
+
+
     public function getHarga() {
         return $this->harga - ($this->harga * $this->diskon / 100);
     }       
@@ -69,9 +108,7 @@ class Game extends Produk {
     $this->waktuMain = $waktuMain;
     }
 
-    public function setDiskon ($diskon) {
-        $this->diskon = $diskon;
-    }
+  
    
 
     public function getInfoProduk() {
@@ -103,9 +140,12 @@ echo "<hr>";
 $produk2->setDiskon(50);
 echo $produk2->getHarga();
 
+echo "<hr>";
 
 
 
+$produk1->setPenulis("Tsana");
+echo $produk1->getPenulis();
 
 
 
