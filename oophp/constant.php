7b0da8748bf2d741c15fdcd492128d1bbaf600b9
Fadlyyyy <fadlyynasah86@f=gmail.com>
2023-09-26 20:48:07 +0700

    about constant

---
+++ oophp/constant.php
@@ -0,0 +1,50 @@
+<?php 
+
+
+// define('NAMA', 'Fardli Yansah');
+
+
+// echo NAMA;
+
+// const UMUR = 21; //define
+
+// echo '<br>'; //const
+
+// echo UMUR;
+
+
+
+
+// class Coba {
+//     const NAMA = 'Fardli';
+// }
+
+
+// echo Coba::NAMA;
+
+
+
+
+
+
+?>
+
+<!-- perbedaan antara define dan const adalah ketika kita ngodingnya mau menggunakan
+konsep oop , menggunakan define tidak bisa disimpan menggunakan class,
+define harus disimpan diluar sebagai konstanta global.
+const bisa dimasukkan/disimpan kedalam class , sehingga bisa digunakan dalam konsep OOP.
+
+magic constant
+
+__LINE__ (akan menampilkan baris, dimana constant ini ditulis)
+__FILE__ (akan menampilkan alamat dari file yang bersangkutan)
+__DIR__ (untuk mengetahui directory dari file yang bersangkutan)
+__FUNCTION__ (untuk menentukan kita lagi ada di function apa)
+__CLASS__ (untuk menentukan kita lagi ada di class apa)
+__TRAIT__
+__METHOD__
+__NAMESPACE__
+
+
+
+  -->
