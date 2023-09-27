22ac13e1270a06576cf711bde59b795bf943162f
Fadlyyyy <fadlyynasah86@f=gmail.com>
2023-09-27 10:28:49 +0700

    abstract class

---
+++ oophp/abstract-class.php
@@ -0,0 +1,41 @@
+<?php 
+
+
+
+
+
+
+
+?>
+
+
+<!-- 
+    abstract class erat kaitannya dengan inheritance
+
+- sebuah kelas yang nantinya tidak dapat diinstansiasi, tidak bisa membuat object dari class abstract.
+- bisa disebut kelas 'abstrak'
+- mendefinisika interface untuk kelas lain yang menjadi turunannya
+- berperan sebagai 'kerangka dasar' untuk kelas turunannya
+- memiliki minimal 1 method abstrak
+- digunakan dalam 'pewarisan' atau inheritance untuk 'memaksakan' implementasu method abstak yang sama untuk semua kelas turunannya.
+
+abstract class (2)
+- semua kelas turunan, harus mengimplementasikan method abstrak yang ada dikelas abstraknya
+- kelas abstrak boleh memiliki property. method reguler
+- kelas abstrak boleh memiliki property / static method
+
+contoh kelas abstrak
+- class mobil extends kendaraan
+- class laptop extends komputer
+- class email extends komunikasi
+...
+
+kenapa menggunakan kelas abstrak
+- merepresentasikan ide atau konsep dasar
+- "composition over inheritance"
+- salah satu cara menerapkan polimorphism
+- sentralisasi logic
+- mempermudah pengerjaan tim
+
+
+-->
